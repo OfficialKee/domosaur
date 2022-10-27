05582c2f1d18cb21087c3f687520c1063631e15c
Kee <shikee.maloney@codeimmersives.com>
2022-10-27 10:20:17 -0400

    all

---
+++ main.js
@@ -0,0 +1,42 @@
+let spanMess = document.querySelector('.mess-with-me');
+spanMess.style.fontSize = '40px'
+
+let paragraphMess = document.querySelector('p.mess-with-me');
+paragraphMess.style.background = 'green';
+
+let hideMe = document.querySelector('#hide-me');
+hideMe.style.display = 'none'
+
+
+let firstDInosaur = document.querySelector('#triceratops');
+firstDInosaur.style.width = '324px';
+
+
+//EVent Listeners
+
+spanMess.addEventListener('click',function(){
+    spanMess.style.color = 'orange';
+})
+
+
+firstDInosaur.addEventListener('click',function(){
+    firstDInosaur.style.border = '3px solid red'
+});
+
+
+let featherDino = document.querySelector('#feathers');
+featherDino.addEventListener('click',function(){
+    featherDino.style.opacity='.5'})
+
+let roww=document.querySelector('#row');
+
+let switchBackground = document.querySelector('#toggle');
+switchBackground.addEventListener('click',function(){
+    roww.style.background='black';
+});
+
+let bigger = document.querySelector('#biggify');
+
+bigger.addEventListener('mouseover',function(){
+    bigger.style.width = '200px';
+})
